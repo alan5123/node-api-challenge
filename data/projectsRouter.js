b7198f4c9da3583d97d07f1377110c32f5194424
alan5123 <alanparker5123@gmail.com>
2020-04-10 12:35:35 -0400

    can add projects

--- data/projectsRouter.js
+++ data/projectsRouter.js
@@ -48,7 +48,16 @@ router
         })
       })
 
-      
+      router
+      .post('/',(req,res) => {
+          Projects.insert(req.body)
+          .then(project => {
+            res.status(200).json(project)
+          })
+          .catch(err => {
+            res.status(500).json({ message: "Error, could not add project" });
+          });
+      })
 
 
 
